402831a4dbd00b7f9dce8060eb34537e9a247591
MH RONY <78216965+dev-mhrony@users.noreply.github.com>
2023-03-04 13:10:21 +0600

    Last Update - 04-03-2023

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(
+    MaterialApp(
+      title: "Code Camp BD",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Center(
+            child: Text("Code Camp Bd"),
+          ),
+        ),
+        body: Center(
+          child: Text("Hello Code Camp BD"),
+        ),
+      ),
+    )
+  );
+}
